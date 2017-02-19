7d194a94633c14040d409b4b937baf549f2ac290
shybyte <ein@volloeko.de>
2017-02-19 08:16:33 +0100

    Added portmidi + monitor_all.rs example

---
+++ README.md
@@ -0,0 +1,2 @@
+# Rust Midi Patcher
+Control (the effects of) one midi instrument with another one. Rust version.
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,49 @@
+extern crate portmidi as pm;
+
+use std::time::Duration;
+use std::sync::mpsc;
+use std::thread;
+
+fn print_devices(pm: &pm::PortMidi) {
+    for dev in pm.devices().unwrap() {
+        println!("{}", dev);
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let context = pm::PortMidi::new().unwrap();
+    print_devices(&context);
+
+    let timeout = Duration::from_millis(10);
+    const BUF_LEN: usize = 1024;
+    let (tx, rx) = mpsc::channel();
+
+    let in_devices: Vec<pm::DeviceInfo> = context.devices()
+        .unwrap()
+        .into_iter()
+        .filter(|dev| dev.is_input())
+        .collect();
+    let in_ports: Vec<pm::InputPort> = in_devices.into_iter()
+        .filter_map(|dev| {
+            context.input_port(dev, BUF_LEN)
+                .ok()
+        })
+        .collect();
+    thread::spawn(move || {
+        loop {
+            for port in &in_ports {
+                if let Ok(Some(events)) = port.read_n(BUF_LEN) {
+                    tx.send((port.device(), events)).unwrap();
+                }
+            }
+            thread::sleep(timeout);
+        }
+    });
+
+    loop {
+        let (device, events) = rx.recv().unwrap();
+        for event in events {
+            println!("[{}] {:?}", device, event);
+        }
+    }
 }
