8976d9fe1f46ba876af88fa1e9465ed1e8dcb8bb
shybyte <ein@volloeko.de>
2017-03-06 08:46:12 +0100

    Added THROUGH_PORT

--- src/midi_devices.rs
+++ src/midi_devices.rs
@@ -1,5 +1,6 @@
 #![allow(dead_code)]
 
+pub const THROUGH_PORT: &'static str = "Through";
 pub const VMPK: &'static str = "VMPK";
 pub const USB_MIDI_ADAPTER: &'static str = "USB";
 pub const SAMPLE_PAD: &'static str = "SamplePad";
@@ -8,3 +9,6 @@ pub const STEP12: &'static str = "12Step";
 
 pub const DEFAULT_IN_DEVICE: &'static str = USB_MIDI_ADAPTER;
 pub const DEFAULT_OUT_DEVICE: &'static str = USB_MIDI_ADAPTER;
+
+//pub const DEFAULT_IN_DEVICE: &'static str = VMPK;
+//pub const DEFAULT_OUT_DEVICE: &'static str = THROUGH_PORT;
