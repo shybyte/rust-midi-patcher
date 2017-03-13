f86fb0655050bbe3c9e24d8d505f33e03aed4711
shybyte <ein@volloeko.de>
2017-03-13 07:09:12 +0100

    Stop playing notes on quiting

--- src/effect.rs
+++ src/effect.rs
@@ -4,6 +4,7 @@ use std::sync::{Arc, Mutex, mpsc};
 use std::sync::mpsc::{Sender};
 use std::time::Duration;
 use std::thread;
+use std::collections::HashSet;
 use absolute_sleep::AbsoluteSleep;
 
 #[derive(Copy, Clone, Eq, PartialEq, Debug)]
@@ -23,7 +24,8 @@ pub struct NoteSequencer {
     notes: Arc<Vec<u8>>,
     velocity: u8,
     time_per_note: Duration,
-    sender: Option<Sender<ThreadCommand>>
+    sender: Option<Sender<ThreadCommand>>,
+    playing_notes: Arc<Mutex<HashSet<u8>>>,
 }
 
 impl NoteSequencer {
@@ -35,6 +37,7 @@ impl NoteSequencer {
             velocity: velocity,
             time_per_note: time_per_note,
             sender: None,
+            playing_notes: Arc::new(Mutex::new(HashSet::new()))
         }
     }
 }
@@ -48,6 +51,7 @@ impl Effect for NoteSequencer {
         let (tx, rx) = mpsc::channel();
         self.sender = Some(tx);
         let notes = self.notes.clone();
+        let playing_notes = self.playing_notes.clone();
         let velocity = self.velocity;
         let time_per_note = self.time_per_note;
         let mut absolute_sleep = absolute_sleep;
@@ -55,25 +59,16 @@ impl Effect for NoteSequencer {
             println!("start sequence = {:?}", midi_message);
 
             for &note in notes.iter() {
-//                println!("play note = {:?}", note);
+                //                println!("play note = {:?}", note);
 
-                let note_on = MidiMessage {
-                    status: 0x90,
-                    data1: note,
-                    data2: velocity,
-                };
+                playing_notes.lock().unwrap().insert(note);
+                play_note_on(&mut output_port_mutex, note, velocity);
 
-                send_midi(&mut output_port_mutex, note_on);
 
                 absolute_sleep.sleep(time_per_note / 2);
 
-                let note_off = MidiMessage {
-                    status: 0x80,
-                    data1: note,
-                    data2: 0x40,
-                };
-
-                send_midi(&mut output_port_mutex, note_off);
+                play_note_off(&mut output_port_mutex, note);
+                playing_notes.lock().unwrap().remove(&note);
 
                 let r = rx.try_recv();
                 if let Ok(ThreadCommand::Stop) = r {
@@ -102,6 +97,35 @@ impl Effect for NoteSequencer {
     fn is_running(&self) -> bool {
         self.sender.is_some()
     }
+}
+
+impl Drop for NoteSequencer {
+    fn drop(&mut self) {
+        for &note in self.playing_notes.lock().unwrap().iter() {
+            play_note_off(&mut self.output_port, note);
+        }
+    }
+}
+
+
+fn play_note_on(output_port_mutex: &mut Arc<Mutex<OutputPort>>, note: u8, velocity: u8) {
+    let note_on = MidiMessage {
+        status: 0x90,
+        data1: note,
+        data2: velocity,
+    };
+
+    send_midi(output_port_mutex, note_on);
+}
+
+fn play_note_off(output_port_mutex: &mut Arc<Mutex<OutputPort>>, note: u8) {
+    let note_off = MidiMessage {
+        status: 0x80,
+        data1: note,
+        data2: 0x40,
+    };
+
+    send_midi(output_port_mutex, note_off);
 }
 
 fn send_midi(output_port_mutex2: &mut Arc<Mutex<OutputPort>>, m: MidiMessage) {
