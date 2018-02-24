dc19d712c794ea7662a02223a17f60a474996469
shybyte <ein@volloeko.de>
2018-02-24 18:26:53 +0100

    Improved Harmony Drum
    * note_duration
    * debounce_duration
    * reset_duration

--- src/effects/harmony_drum.rs
+++ src/effects/harmony_drum.rs
@@ -1,6 +1,6 @@
 use pm::{MidiMessage, DeviceInfo};
 use std::sync::{Arc, Mutex};
-use std::time::Duration;
+use std::time::{Duration, Instant};
 use std::thread;
 use absolute_sleep::AbsoluteSleep;
 use effects::effect::{Effect, MonoGroup};
@@ -16,17 +16,27 @@ pub struct HarmonyDrum {
     harmony_input_device: String,
     output_device: String,
     current_note: u8,
+    note_duration: Duration,
+    debounce_duration: Duration,
+    reset_duration: Duration,
+    last_timestamp: Instant,
 }
 
 impl HarmonyDrum {
-    pub fn new(harmony_input_device: &str, output_device: &str, note_range: (u8, u8), notes: Vec<u8>) -> HarmonyDrum {
+    pub fn new(harmony_input_device: &str, output_device: &str,
+               note_range: (u8, u8), notes: Vec<u8>, note_duration: Duration, debounce_duration: Duration,
+               reset_duration: Duration) -> HarmonyDrum {
         HarmonyDrum {
             note_range,
             notes_index: 0,
             notes,
             harmony_input_device: harmony_input_device.to_string(),
             output_device: output_device.to_string(),
-            current_note: C3
+            current_note: C3,
+            note_duration,
+            debounce_duration,
+            reset_duration,
+            last_timestamp: Instant::now(),
         }
     }
 }
@@ -42,15 +52,29 @@ impl Effect for HarmonyDrum {
 
     fn start(&mut self, midi_message: MidiMessage, absolute_sleep: AbsoluteSleep,
              virtual_midi_out: &Arc<Mutex<VirtualMidiOutput>>) {
+        let timestamp = Instant::now();
+        let duration_since_last_event = timestamp - self.last_timestamp;
+        self.last_timestamp = timestamp;
+
+        if { duration_since_last_event < self.debounce_duration} {
+            return;
+        } else if { duration_since_last_event > self.reset_duration} {
+            self.notes_index = 0;
+        }
+
+        eprintln!("Note Index {:?}", self.notes_index);
+
+        self.last_timestamp = Instant::now();
         let mut absolute_sleep = absolute_sleep;
         let played_note = self.current_note + self.notes[self.notes_index];
         self.notes_index = (self.notes_index + 1) % self.notes.len();
         play_note_on(&self.output_device, virtual_midi_out, played_note, midi_message.data2);
         let virtual_midi_out_clone = Arc::clone(virtual_midi_out);
         let out_device = self.output_device.clone();
+        let note_duration = self.note_duration.clone();
         thread::spawn(move || {
             println!("play harmony drum {:?} {:?}", midi_message, played_note);
-            absolute_sleep.sleep(Duration::from_millis(100));
+            absolute_sleep.sleep(note_duration);
             play_note_off(&out_device, &virtual_midi_out_clone, played_note);
         });
     }
--- src/songs/harmony_drum_test.rs
+++ src/songs/harmony_drum_test.rs
@@ -1,3 +1,4 @@
+use std::time::Duration;
 use patch::Patch;
 use trigger::Trigger;
 use effects::harmony_drum::HarmonyDrum;
@@ -12,11 +13,22 @@ pub fn create_harmony_drum_test_song(_config: &Config) -> Patch {
         //        (Box::new(Trigger::new(SAMPLE_PAD, 45)), Box::new(note_seq(vec![]))),
         (
             Box::new(Trigger::new(SAMPLE_PAD, 45)),
-            Box::new(HarmonyDrum::new(USB_MIDI_ADAPTER, USB_MIDI_ADAPTER, (C2, D3), vec![0]))
+            Box::new(HarmonyDrum::new(USB_MIDI_ADAPTER, USB_MIDI_ADAPTER, (C2, D3), vec![0],
+                                      Duration::from_millis(100), Duration::from_millis(0), Duration::from_secs(3600)))
         ),
         (
             Box::new(Trigger::new(SAMPLE_PAD, 51)),
-            Box::new(HarmonyDrum::new(USB_MIDI_ADAPTER, USB_MIDI_ADAPTER, (C2, D3), vec![0, 7, 12]))
+            Box::new(HarmonyDrum::new(
+                USB_MIDI_ADAPTER, USB_MIDI_ADAPTER, (C2, C5), vec![7, 12, 19], Duration::from_millis(100), Duration::from_millis(0), Duration::from_secs(3600)))
         ),
-    ], 0, None)
+//        (
+//            Box::new(Trigger::new(SAMPLE_PAD, 38)),
+//            Box::new(HarmonyDrum::new(
+//                USB_MIDI_ADAPTER, USB_MIDI_ADAPTER, (C2, C5), vec![4, 7, 4, 0, 4, 7, 16, 12], Duration::from_millis(100), Duration::from_millis(200), Duration::from_secs(2)))
+//        ),
+//        (
+//            Box::new(Trigger::new(KBOARD, 48)),
+//            Box::new(HarmonyDrum::new(KBOARD, THROUGH_PORT, (D4, C5), vec![7, 12, 19]))
+//        ),
+    ], 45, None)
 }
