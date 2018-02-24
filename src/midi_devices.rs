676edc3fa0e99e61e75ac4acd182f7226f757f5b
shybyte <ein@volloeko.de>
2018-02-24 18:24:38 +0100

    More midi_device constants

--- src/midi_devices.rs
+++ src/midi_devices.rs
@@ -6,6 +6,8 @@ pub const USB_MIDI_ADAPTER: & str = "USB";
 pub const SAMPLE_PAD: & str = "SamplePad";
 pub const MICRO_KEY: &str = "micro";
 pub const STEP12: &str = "12Step";
+pub const K_BOARD: &str = "K-B";
+pub const EXPRESS_PEDAL: &str = "Express";
 
 pub const DEFAULT_IN_DEVICE: &str = USB_MIDI_ADAPTER;
 pub const DEFAULT_OUT_DEVICE: &str = USB_MIDI_ADAPTER;
